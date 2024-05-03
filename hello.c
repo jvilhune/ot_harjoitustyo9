2e9e16faf81ecc2788a2f46b61e9b5c87c4588fd
Jukka Vilhunen <jukka.vilhunen@hotmail.com>
2024-05-03 09:33:28 +0300

    hello.c file added

---
+++ hello.c
@@ -0,0 +1,32 @@
+
+include <stdio.h>
+long calculateSum(long a, lon b);
+
+int main(void)
+{
+	long retVal = 0;
+
+	printf("Content-type: text/html; charset=iso-8859-1\n\n");
+	printf("<html>");
+	printf("<body>");
+	printf("<BR>");
+	printf("Hello World\n");
+	printf("<BR>");
+	printf("</body>");
+	printf("</html>");
+
+	retVal = calculateSum(20, 30);
+	printf("retVal = ", retVal);
+       
+	return 0;
+}
+
+long calculateSum(long a, lon b)
+{
+	long retVal = 0;
+
+	retVal = a + b;
+	return retVal;
+}
+
+
